b06876b6ede2514632df04b2454708ed39a421ed
csch0 <schoning.christoph@gmail.com>
2025-01-09 06:41:47 +0100

    updated oil config

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,21 +1,25 @@
 return {
 	"stevearc/oil.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
+	opts = {
+		default_file_explorer = true,
+		delete_to_trash = true,
+		skip_confirm_for_simple_edits = true,
+		columns = { "icon", "permissions", "size", "mtime" },
+		watch_for_changes = true,
+		keymaps = {
+			["q"] = "actions.close",
+		},
+		view_options = {
+			show_hidden = true,
+			is_always_hidden = function(name, _)
+				return name == ".." or name == ".git"
+			end,
+		},
+	},
+	config = function(_, opts)
 		local oil = require("oil")
-		oil.setup({
-			default_file_explorer = true,
-			delete_to_trash = true,
-			skip_confirm_for_simple_edits = false,
-			columns = { "icon", "permissions", "size", "mtime" },
-			watch_for_changes = true,
-			keymaps = {
-				["q"] = "actions.close",
-			},
-			view_options = {
-				show_hidden = true,
-			},
-		})
+		oil.setup(opts)
 		local keymap = vim.keymap
 		keymap.set("n", "-", oil.open, { desc = "Open Oil" })
 	end,
