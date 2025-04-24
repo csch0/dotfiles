c07f6eea3fe784034e5d4c5e7bafba608cab87d3
csch0 <schoning.christoph@gmail.com>
2025-04-24 14:14:28 +0200

    updated lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,5 +1,48 @@
 return {
-	{
-		"neovim/nvim-lspconfig",
+	"neovim/nvim-lspconfig",
+	opts = {
+		servers = {
+			lua_ls = {
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" },
+						},
+						runtime = {
+							path = {
+								"lua/?.lua",
+								"lua/?/init.lua",
+							},
+							version = "LuaJIT",
+						},
+						telemetry = { enable = false },
+						workspace = {
+							checkThirdParty = false,
+							library = {
+								"${3rd}/luv/library",
+								unpack(vim.api.nvim_get_runtime_file("", true)),
+							},
+						},
+					},
+				},
+			},
+			cssls = {},
+			html = {},
+			svelte = {},
+			tailwindcss = {},
+			ts_ls = {},
+			--
+			pyright = {},
+			ruff = {},
+			--
+			dockerls = {},
+			docker_compose_language_service = {},
+		},
 	},
+	config = function(_, opts)
+		for server, config in pairs(opts.servers) do
+			vim.lsp.config(server, config)
+			vim.lsp.enable(server)
+		end
+	end,
 }
--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,66 +1,12 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		opts = {
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
-				},
+	"williamboman/mason.nvim",
+	opts = {
+		ui = {
+			icons = {
+				package_installed = "✓",
+				package_pending = "➜",
+				package_uninstalled = "✗",
 			},
 		},
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"ruff",
-					"pyright",
-					"cssls",
-					"html",
-					"rust_analyzer",
-					---
-					"dockerls",
-					"docker_compose_language_service",
-				},
-				handlers = {
-					function(server_name)
-						-- local capabilities = require("cmp_nvim_lsp").default_capabilities()
-						local capabilities = require("blink.cmp").get_lsp_capabilities()
-						require("lspconfig")[server_name].setup({ capabilities = capabilities })
-					end,
-					["lua_ls"] = function()
-						require("lspconfig").lua_ls.setup({
-							settings = {
-								Lua = {
-									diagnostics = {
-										globals = { "vim" },
-									},
-									runtime = {
-										path = {
-											"lua/?.lua",
-											"lua/?/init.lua",
-										},
-										version = "LuaJIT",
-									},
-									telemetry = { enable = false },
-									workspace = {
-										checkThirdParty = false,
-										library = {
-											vim.env.VIMRUNTIME,
-											"${3rd}/luv/library",
-											vim.fn.expand("~/.local/share/nvim/lazy/"),
-										},
-									},
-								},
-							},
-						})
-					end,
-				},
-			})
-		end,
 	},
 }
