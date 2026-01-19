bc43d1cd80eefbed0b08c739d6f1199605b23e52
csch0 <schoning.christoph@gmail.com>
2026-01-19 15:51:49 +0100

    updated fzf

--- nvim-nightly/lua/plugins/fzf.lua
+++ nvim-nightly/lua/plugins/fzf.lua
@@ -1,21 +1,30 @@
 vim.pack.add({
-  { src = "https://github.com/ibhagwan/fzf-lua" },
-  { src = "https://github.com/nvim-tree/nvim-web-devicons" }
+	{ src = "https://github.com/ibhagwan/fzf-lua" },
+	{ src = "https://github.com/nvim-tree/nvim-web-devicons" },
 })
 
 require("fzf-lua").setup({
-  winopts = {
-    backdrop = 100,
-    preview = {
-      scrollbar = false,
-      title = false,
-    },
-  },
-  files = {
-    cwd_prompt = false,
-  }
+	winopts = {
+		backdrop = 100,
+		preview = {
+			scrollbar = false,
+			title = false,
+		},
+	},
+	files = {
+		cwd_prompt = false,
+	},
+	ui_select = true,
 })
 
 vim.keymap.set("n", "<leader>ff", "<cmd>FzfLua files<cr>", { desc = "Files" })
 vim.keymap.set("n", "<leader>fh", "<cmd>FzfLua helptags<cr>", { desc = "Neovim Help" })
 vim.keymap.set("n", "<leader>fr", "<cmd>FzfLua oldfiles<cr>", { desc = "Old Files" })
+
+vim.keymap.set({ "n", "v", "i" }, "<C-x><C-f>", function()
+	FzfLua.complete_path()
+end, { silent = true })
+
+vim.keymap.set("n", "<leader>f.", function()
+	require("fzf-lua").files({ cwd = "~/dotfiles/", prompt = "Dotfiles> ", no_header = true })
+end, { desc = "Dotfiles" })
