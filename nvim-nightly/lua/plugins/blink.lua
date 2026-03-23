2b2c0852e841fd28dab2490a104808275e7f2ae5
csch0 <schoning.christoph@gmail.com>
2026-03-23 09:28:09 +0100

    updated blink.cmp

--- nvim-nightly/lua/plugins/blink.lua
+++ nvim-nightly/lua/plugins/blink.lua
@@ -1,5 +1,5 @@
 vim.pack.add({
-	{ src = "https://github.com/Saghen/blink.cmp", version = "v1.9.1" },
+	{ src = "https://github.com/Saghen/blink.cmp", version = "v1.10.1" },
 })
 
 require("blink.cmp").setup({
